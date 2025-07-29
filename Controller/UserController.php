44e8f1f01f2281def4f5e32b6f68627b3b20d535
Gustavo050821 <luisgustavofe2105@gmail.com>
2025-07-29 12:28:27 -0300

    UserController update

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -9,9 +9,56 @@ class UserController
 {
     private $userModel;
 
-    public function __contruct()
+    public function __construct()
     {
-        
+        $this->userModel = new User();
+    }
+
+    public function createUser($user_fullname, $email, $password)
+    {
+        if (empty($user_fullname) or empty($email) or empty($password)) {
+            return false;
+        }
+
+        return $this->userModel->registerUser($user_fullname, $email, $password);
+
+    }
+
+    public function checkUserByEmail($email)
+    {
+        return $this->userModel->getUserByEmail($email);
+    }
+
+    public function login($email, $password)
+    {
+        $user = $this->userModel->getUserByEmail($email);
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['user_fullname'] = $user['user_fullname'];
+            $_SESSION['email'] = $user['email'];
+            var_dump($_SESSION);
+            return true;
+        }
+        return false;
+    }
+}
+
+public function isLoggedIn()
+{
+    return isset($_SESSION['id']);
+}
+
+public function isLoggedIn()
+{
+    return isset($_SESSION['id']);
+}
+
+public function getUserData($id, $user_fullname, $name)
+{
+    return $this->userModel->getUserInfo($id, $user_fullname, $email);
+    {
+        return $this->userModel->getUserInfo($id, $user_fullname, $email);
     }
 }
 ?>
